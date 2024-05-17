5f11d7be5d4f6645305c58f09cab2e7dfa3a5d98
vishrutpanya <vishrutpanya03@gmail.com>
2024-05-18 01:34:31 +0530

    initialize backend and frontend for uploading images

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import axios from "axios";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const [file, setFile] = useState(null);
+  const handleUploadImage = () => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios
+      .post("http://localhost:3000/upload", formData)
+      .then()
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={(e) => e.target.files[0]} />
+      <button onClick={handleUploadImage}>Upload Image</button>
+    </div>
+  );
 }
